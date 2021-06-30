48bb65b859a2e09f18f69cc3eef23b8f71d9d6c6
eshanajain <82397896+eshanajain@users.noreply.github.com>
2021-06-30 21:27:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var images =[
+"https://static.wikia.nocookie.net/barbie/images/3/32/Margaret_Roberts_Dreamhouse_Adventures.png/revision/latest/top-crop/width/360/height/450?cb=20210313000243","https://i.pinimg.com/originals/22/2f/9f/222f9fe1a39db50981b0498595e1160c.png",
+"https://i.pinimg.com/originals/46/10/6f/46106fd1e02cdc508f36f4b5aba29f0e.jpg","https://static.wikia.nocookie.net/barbie-movies/images/e/ec/Chelsea_Princess_Adventure.png/revision/latest?cb=20210211014128","https://i.pinimg.com/736x/be/0b/9d/be0b9da2ec52af61a0bddbc8106c946b.jpg""
+]
+
+var name=["family book","barbie","margreat","george","skipper","chelsy","staicy",]
+
+var i =0;
+function update()
+{
+    i++;
+    var number_of_family_member_in_array = 5
+    if(i > number_of_family_member_in_array)
+{
+    i= 0;
+}
+var updateImage = images[i];
+var updateName = name[i];
+
+}
+ 
